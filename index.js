90f10ba124d44e42c02ba84dc52d6a53159d5116
sobrien-eng <78193145+sobrien-eng@users.noreply.github.com>
2021-06-02 13:15:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require('path');
 const { url } = require('inspector');
 
 const app = express();
+const cookieParser = require('cookie-parser');
+app.use(cookieParser('parser'));
 
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -12,8 +14,9 @@ let urlencodedParser = express.urlencoded({
     extended: true
 });
 
-app.get('/', routes.login);
+app.get('/', routes.welcome);
 app.get('/signup', urlencodedParser, routes.signup);
 app.get('/login', urlencodedParser, routes.login);
+app.get('/login', urlencodedParser, routes.edit);
 
 app.listen(3000);
